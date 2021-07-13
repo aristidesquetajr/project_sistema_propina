eaeff4fe96f703506608e58d47edbed8b9f14dcb
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2021-07-13 18:51:05 +0100

    update not so important

--- src/sistemapropina/SistemaPropina.java
+++ src/sistemapropina/SistemaPropina.java
@@ -11,7 +11,7 @@ public class SistemaPropina {
 
     public static void main(String[] args) {
         // TODO code application logic here
-        new Painel().setVisible(true);
+        new Login().setVisible(true);
     }
     
 }
