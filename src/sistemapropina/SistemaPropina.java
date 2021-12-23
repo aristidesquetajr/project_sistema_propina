88d7c3c951b6cc6af011ef1cdc751810685aa8ec
Aristides Queta AK <63754931+aristidesquetajr@users.noreply.github.com>
2021-12-23 03:25:11 +0100

    Criacao do projecto

---
+++ src/sistemapropina/SistemaPropina.java
@@ -0,0 +1,13 @@
+package sistemapropina;
+
+/**
+ *
+ * @author kashiki
+ */
+public class SistemaPropina {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
