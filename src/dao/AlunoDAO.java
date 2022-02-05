d45f7de5bde768e473cb0b7ff570927b011425b5
Aristides Queta AK <aristidesquetajr@gmail.com>
2022-02-05 17:48:50 +0100

    Criacao do metodo cadastrar aluno

--- src/dao/AlunoDAO.java
+++ src/dao/AlunoDAO.java
@@ -1,7 +1,12 @@
 package dao;
 
+import beans.Aluno;
+import beans.Classe;
+import beans.Pessoa;
 import conexao.Conexao;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class AlunoDAO {
     private Conexao conexao;
@@ -12,5 +17,18 @@ public class AlunoDAO {
         this.conn = this.conexao.getConnection();
     }
     
-    
+    public boolean cadastro(Pessoa pessoa, Aluno aluno, Classe classe) {
+        String sql = "INSERT INTO Aluno (fkPessoa, fkClasse, numProcesso, numEstudante) values (?, ?, ?, ?)";
+        try {
+            PreparedStatement stmt = this.conn.prepareStatement(sql);
+            stmt.setInt(1, pessoa.getIdPessoa());
+            stmt.setInt(2, classe.getIdClasse());
+            stmt.setInt(3, aluno.getNumProcesso());
+            stmt.setInt(4, aluno.getNumEstudantes());
+            return stmt.execute();
+        } catch(SQLException e) {
+            System.out.println("Erro no cadastro: " + e.getMessage());
+        }
+        return false;
+    }
 }
