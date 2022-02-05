92bf13696fddd07b948497334e1c4cc1a5a365e5
Aristides Queta AK <aristidesquetajr@gmail.com>
2022-02-05 18:26:13 +0100

    Criacao do metodo para mostrar os alunos

--- src/dao/AlunoDAO.java
+++ src/dao/AlunoDAO.java
@@ -11,6 +11,7 @@ import java.sql.SQLException;
 public class AlunoDAO {
     private Conexao conexao;
     private Connection conn;
+    private PreparedStatement stmt;
     
     public AlunoDAO() {
         this.conexao = new Conexao();
@@ -20,15 +21,24 @@ public class AlunoDAO {
     public boolean cadastro(Pessoa pessoa, Aluno aluno, Classe classe) {
         String sql = "INSERT INTO Aluno (fkPessoa, fkClasse, numProcesso, numEstudante) values (?, ?, ?, ?)";
         try {
-            PreparedStatement stmt = this.conn.prepareStatement(sql);
-            stmt.setInt(1, pessoa.getIdPessoa());
-            stmt.setInt(2, classe.getIdClasse());
-            stmt.setInt(3, aluno.getNumProcesso());
-            stmt.setInt(4, aluno.getNumEstudante());
+            this.stmt = this.conn.prepareStatement(sql);
+            this.stmt.setInt(1, pessoa.getIdPessoa());
+            this.stmt.setInt(2, classe.getIdClasse());
+            this.stmt.setInt(3, aluno.getNumProcesso());
+            this.stmt.setInt(4, aluno.getNumEstudante());
             return stmt.execute();
         } catch(SQLException e) {
             System.out.println("Erro no cadastro: " + e.getMessage());
         }
         return false;
     }
+    
+    public void mostrarAlunos() {
+        String sql = "SELECT * FROM Pessoa AS pes LEFT JOIN Aluno AS alu ON (pes.idPessoa = alu.fkPessoa)";
+        try {
+            this.stmt = this.conn.prepareStatement(sql);
+            stmt.execute();
+        } catch (Exception e) {
+        }
+    }    
 }
